5f116bcf9b1aec917563a94d2dadeb42bb712f92
aikchun <aikchun616@gmail.com>
2018-01-23 17:09:36 +0800

    Implemented insert_or_update function for user in the database

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -5,7 +5,26 @@ defmodule Discuss.AuthController do
   alias Discuss.User
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "githbub"}
-    User.changeset(%User{}, user_params)
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+
+    insert_or_update_user(changeset)
+
+    signin(conn, changeset)
+
   end
+
+  defp signin(conn, changeset) do
+
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil -> 
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
+
 end
