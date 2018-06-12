d6e2b24220e28bb68411aed657854fab26112614
aikchun <aikchun616@gmail.com>
2018-06-12 12:42:46 +0800

    Successfully created a channel which the template is able to receive newly added comments

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -6,8 +6,13 @@ socket.connect()
 const createSocket = (topicId) => {
 	let channel = socket.channel(`comments:${topicId}`, {})
 	channel.join()
-		.receive("ok", resp => { console.log("Joined successfully", resp) })
+		.receive("ok", resp => {
+			renderComments(resp.comments);
+			console.log("Joined successfully", resp) 
+		})
 		.receive("error", resp => { console.log("Unable to join", resp) })
+
+	channel.on(`comments:${topicId}:new`, renderComment)
 
 	document.querySelector('button').addEventListener('click', () => {
 		const content = document.querySelector('textarea').value;
@@ -16,4 +21,26 @@ const createSocket = (topicId) => {
 	});
 }
 
+function renderComments(comments) {
+	const renderedComments = comments.map(comment => {
+		return commentTemplate(comment)
+	})
+	document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
+
+function renderComment(event) {
+	const renderedComment =  commentTemplate(event.comment);
+
+	document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+
+function commentTemplate(comment) {
+	return `
+		<li class="collection-item">
+			${comment.content}
+		</li>
+	`
+
+}
 window.createSocket = createSocket;
